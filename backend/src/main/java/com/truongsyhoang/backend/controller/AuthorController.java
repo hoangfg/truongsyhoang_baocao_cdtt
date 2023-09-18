c3e97b7d9fcf50b01e6c2c2ed9857a2c64b58bcb
hoangfg <121874187+hoangfg@users.noreply.github.com>
2023-09-18 17:35:22 +0700

    add download image for author

--- backend/src/main/java/com/truongsyhoang/backend/controller/AuthorController.java
+++ backend/src/main/java/com/truongsyhoang/backend/controller/AuthorController.java
@@ -1,26 +1,34 @@
 package com.truongsyhoang.backend.controller;
 
+import java.net.http.HttpRequest;
 import java.text.Normalizer;
 import java.time.LocalDate;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.truongsyhoang.backend.domain.Author;
 import com.truongsyhoang.backend.dto.AuthorDTO;
+import com.truongsyhoang.backend.exception.FileNotFoundException;
+import com.truongsyhoang.backend.exception.FileStorageException;
 import com.truongsyhoang.backend.service.AuthorService;
 import com.truongsyhoang.backend.service.FileStorageService;
 import com.truongsyhoang.backend.service.MapValidationErrorService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -34,18 +42,41 @@ public class AuthorController {
     @Autowired
     MapValidationErrorService mapValidationErrorService;
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, 
-    MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-    MediaType.MULTIPART_FORM_DATA_VALUE},
-    produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            MediaType.MULTIPART_FORM_DATA_VALUE }, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> create(@Valid @ModelAttribute AuthorDTO dto, BindingResult result) {
-        ResponseEntity<?> responseEntity = mapValidationErrorService.mapValidationFields(result); 
+        ResponseEntity<?> responseEntity = mapValidationErrorService.mapValidationFields(result);
+        if (responseEntity != null) {
+            return responseEntity;
+        }
         Author entity = authorService.insert(dto);
         dto.setId(entity.getId());
         dto.setImage(entity.getImage());
-        
+
         return new ResponseEntity<>(entity, HttpStatus.CREATED);
     }
 
-   
+    @GetMapping("/logo/{fileName:.+}")
+    public ResponseEntity<?> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        Resource resource = fileStorageService.loadAuthorFileAResource(fileName);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            throw new FileStorageException("could not delermine file type.");
+        }
+        if (contentType == null) {
+            contentType = "application/octer-stream";
+        }
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=\""
+                        + resource.getFilename() + "\"")
+                .body(resource);
+
+    }
+
 }
